8aa2ba3275a59d0c38312ad04a4fe0747ca3569e
Kim Yu Na <yunasun0920@gmail.com>
2022-08-24 18:22:24 +0900

    #4 구조체 실습

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -10,6 +10,7 @@ namespace CSClass2
     {
         static void Main(string[] args)
         {
+            // 제네릭
             Wanted<string> wantedString = new Wanted<string>("String");
             Wanted<int> wantedInt = new Wanted<int>(52273);
             Wanted<double> wantedDouble = new Wanted<double>(52.273);
@@ -18,10 +19,12 @@ namespace CSClass2
             Console.WriteLine(wantedInt.Value);
             Console.WriteLine(wantedDouble.Value);
 
+            // 인덱서
             Products p = new Products();
             Console.WriteLine(p[4]);
             p[4] = 5;
 
+            // out 키워드
             Console.WriteLine("숫자 입력 : ");
             int output;
             bool result = int.TryParse(Console.ReadLine(), out output);
@@ -39,12 +42,79 @@ namespace CSClass2
             Console.WriteLine("현재 좌표 : (" + x + ", " + y + ")");
             NextPos(x, y, vx, vy, out x, out y);
             Console.WriteLine("다음 좌표 : (" + x + ", " + y + ")");
+
+            // 구조체
+            Point point;
+            point.x = 10;
+            point.y = 10;
+            point = new Point(3, 5);
+            Console.WriteLine(point.x + " / " + point.y);
+
+            PointClass pointClassA = new PointClass(10, 20);
+            PointClass pointClassB = pointClassA;
+            pointClassB.x = 100; pointClassB.y = 200;
+            Console.WriteLine(pointClassA.x + " / " + pointClassA.y);
+            Console.WriteLine(pointClassB.x + " / " + pointClassB.y);
+
+            PointStruct pointStructA = new PointStruct(10, 20);
+            PointStruct pointStructB = pointStructA;
+            pointStructB.x = 100; pointStructB.y = 200;
+            Console.WriteLine(pointStructA.x + " / " + pointStructA.y);
+            Console.WriteLine(pointStructB.x + " / " + pointStructB.y);
         }
 
         static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
         {
             rx = x + vx;
             ry = y + vy;
+        }
+
+        struct Point
+        {
+            public int x;
+            public int y;
+            public string testA;
+            public string testB;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = "초기화";
+                this.testB = "초기화";
+            }
+
+            public Point(int x, int y, string test)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = test;
+                this.testB = test;
+            }
+        }
+
+        class PointClass
+        {
+            public int x;
+            public int y;
+
+            public PointClass(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        struct PointStruct
+        {
+            public int x;
+            public int y;
+
+            public PointStruct(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
         }
     }
 }
